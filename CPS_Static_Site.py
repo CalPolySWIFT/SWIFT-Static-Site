38f4b24deae1e9b505f7cca5cd50b6c037b8a187
fragtastic <fragtastic@users.noreply.github.com>
2015-09-13 18:52:39 -0700

    Moved individual pages. Pages are now located directly in templates/ rather than templates/pages. Fixes refactoring problems and locating files in the IDE.

--- CPS_Static_Site.py
+++ CPS_Static_Site.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("pages/index.html")
+    return render_template("index.html")
 
 @app.route('/<page>')
 def page(page):
-    return render_template("pages/"+page)
+    return render_template(page)
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("pages/404.html")
+    return render_template("404.html")
 
 @app.errorhandler(500)
 def error_500(error):
-    return render_template("pages/404.html")
+    return render_template("404.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
