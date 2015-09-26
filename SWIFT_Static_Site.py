f8a2a924dad09712ff130b895ce4e8580d7fc2af
fragtastic <fragtastic@users.noreply.github.com>
2015-09-25 21:02:50 -0700

    Added SMTP error sending

--- SWIFT_Static_Site.py
+++ SWIFT_Static_Site.py
@@ -3,18 +3,23 @@ import urllib
 import json
 
 app = Flask(__name__)
+app.debug = False
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/<page>')
 def page(page):
     return render_template(page)
 
-@app.route("/getcalendar")
+
 def get_calendar():
-    response = urllib.request.urlopen("https://www.googleapis.com/calendar/v3/calendars/calpolyswift.org_20ccnpmk81dvsn70peppejgf48%40group.calendar.google.com/events?maxResults=5&timeMin=2015-09-05T07%3A23%3A12.000Z&ctz=America/Los_Angeles&singleEvents=true&orderBy=startTime&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs").read().decode('utf-8')
+    response = urllib.request.urlopen(
+        "https://www.googleapis.com/calendar/v3/calendars/calpolyswift.org_20ccnpmk81dvsn70peppejgf48%40group.calendar.google.com/events?maxResults=5&timeMin=2015-09-05T07%3A23%3A12.000Z&ctz=America/Los_Angeles&singleEvents=true&orderBy=startTime&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs").read().decode(
+        'utf-8')
     jdata = json.loads(response)
     hdata = ""
     for item in jdata['items']:
@@ -29,26 +34,38 @@ def get_calendar():
         day = date[2]
         year = date[0]
         # 2015-09-29T12:00:00-07:00
-        hdata += "<span class=\"month\">"+month+"</span>"
-        hdata += "<span class=\"date-number\">"+day+"</span>"
+        hdata += "<span class=\"month\">" + month + "</span>"
+        hdata += "<span class=\"date-number\">" + day + "</span>"
         hdata += "</p>"
         hdata += "<div class=\"details\">"
-        hdata += "<h2 class=\"title\">"+item['summary']+"</h2>"
-        hdata += "<p class=\"time\"><i class=\"fa fa-clock-o\"></i>"+time+"</p>"
-        #hdata += "<p class=\"location\"><i class=\"fa fa-map-marker\"></i>"+item['location']+"</p>"
+        hdata += "<h2 class=\"title\">" + item['summary'] + "</h2>"
+        hdata += "<p class=\"time\"><i class=\"fa fa-clock-o\"></i>" + time + "</p>"
+        # hdata += "<p class=\"location\"><i class=\"fa fa-map-marker\"></i>"+item['location']+"</p>"
         hdata += "</div>"
         hdata += "</div>"
     return hdata
 
+
 app.jinja_env.globals.update(get_calendar=get_calendar)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html")
 
+
 @app.errorhandler(500)
 def error_500(error):
     return render_template("404.html")
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    if not app.debug:
+        import logging
+        from logging.handlers import SMTPHandler
+
+        ADMINS = ['fragtastic@gmail.com']
+        mail_handler = SMTPHandler('127.0.0.1', 'noreply@calpolyswift.org', ADMINS, 'SWIFT Static Site Failed')
+        mail_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(mail_handler)
+    app.run(host='0.0.0.0')
