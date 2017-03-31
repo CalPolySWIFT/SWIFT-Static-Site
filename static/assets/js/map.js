71f733e33e7daa17b800860736683de5701cfe3d
frag <fragtastic@users.noreply.github.com>
2017-03-30 17:30:58 -0700

    Updated general meeting room.

--- static/assets/js/map.js
+++ static/assets/js/map.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function(){
         lng: -117.8196481,
         title: 'Address',
         infoWindow: {
-            content: '<h5 class="title">SWIFT Meeting Room CLA 98P 2 007 @ Cal Poly Pomona</h5><p><span class="region">3801 W Temple Ave</span><br><span class="postal-code">Pomona, CA 91768</span><br><span class="country-name">United States</span></p>'
+            content: '<h5 class="title">SWIFT General Meeting Room: CBA 162 room 1001 @ Cal Poly Pomona</h5><p><span class="region">3801 W Temple Ave</span><br><span class="postal-code">Pomona, CA 91768</span><br><span class="country-name">United States</span></p>'
         }
 
     });
