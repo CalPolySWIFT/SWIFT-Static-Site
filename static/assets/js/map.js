898b398f60e3c0253c37629bd03f5081ba4810a5
fragtastic <fragtastic@users.noreply.github.com>
2018-01-09 10:50:28 -0800

    Updated map to include proper location on meeting room and telcolab location

--- static/assets/js/map.js
+++ static/assets/js/map.js
@@ -16,13 +16,21 @@ jQuery(document).ready(function(){
         
     });
     map.addMarker({
-        lat: 34.0597805,
-        lng: -117.8196481,
+        lat: 34.061297,
+        lng: -117.819525,
         title: 'Address',
         infoWindow: {
             content: '<h5 class="title">SWIFT General Meeting Room: CBA 162 room 1001 @ Cal Poly Pomona</h5><p><span class="region">3801 W Temple Ave</span><br><span class="postal-code">Pomona, CA 91768</span><br><span class="country-name">United States</span></p>'
         }
 
+    });
+    map.addMarker({
+        lat: 34.059670,
+        lng: -117.819747,
+        title: 'Address',
+        infoWindow: {
+            Content: '<h5 class="title">SWIFT Telecom Lab: CLA 98C 4-36 @ Cal Poly Pomona</h5><p><span class="region">3801 W Temple Ave</span><br><span class="postal-code">Pomona, CA 91768</span><br><span class="country-name">United States</span></p>'
+        }
     });
 
 });
